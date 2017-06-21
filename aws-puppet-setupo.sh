925f4f44c6bedf37ed194303de2abc31706372bd
Bhanu <bhanuandchandra@gmail.com>
2017-06-21 10:08:08 +0530

    Create aws-puppet-setupo.sh

---
+++ aws-puppet-setupo.sh
@@ -0,0 +1,14 @@
+#https://www.digitalocean.com/community/tutorials/how-to-install-puppet-4-in-a-master-agent-setup-on-centos-7
+
+sudo bash
+timedatectl list-timezones
+date
+sudo timedatectl set-timezone Asia/Kolkata
+yum -y install ntp
+sudo ntpdate pool.ntp.org
+
+systemctl enable ntpd
+sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum -y install puppetserver
+
+#edit /etc/sysconfig/puppetserver to change JAVA_ARGS="-Xms512m -Xmx512m -XX:MaxPermSize=256m" to appropritate size
