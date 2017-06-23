f16201a2dc8ec8b540572407e851aac29bb640de
Bhanu <bhanuandchandra@gmail.com>
2017-06-23 09:36:36 +0530

    Update aws-puppet-agent-setup.sh

--- aws-puppet-agent-setup.sh
+++ aws-puppet-agent-setup.sh
@@ -3,3 +3,4 @@ sudo bash
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+/opt/puppetlabs/bin/puppet agent --server ip-172-31-8-199.ap-south-1.compute.internal --waitforcert 60 --test  # <ip address of the master>
