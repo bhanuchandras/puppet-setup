d95b907c28d3d06a6e85b30ad0195982b278bb40
bhanuchandras <bhanuandchandra@gmail.com>
2018-10-18 21:55:17 +0530

    bootstrap changes to add agent and master details

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,7 @@ sudo yum update -y
 sudo rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
 sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 sudo yum install puppet -y
+sudo echo "192.168.10.22 bhanu-master" >> /etc/hosts
+sudo echo "192.168.10.23 bhanu-agent" >> /etc/hosts
+sudo echo "[agent]"  >> /etc/puppetlabs/puppet/puppet.conf
+sudo echo "server=bhanu-master" >> /etc/puppetlabs/puppet/puppet.conf
--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -2,3 +2,8 @@
 sudo yum update -y
 sudo rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
 sudo yum install puppet puppetserver -y
+sudo echo "192.168.10.22 bhanu-master" >> /etc/hosts
+sudo echo "192.168.10.23 bhanu-agent" >> /etc/hosts
+sudo echo "[agent]"  >> /etc/puppetlabs/puppet/puppet.conf
+sudo echo "server=bhanu-master" >> /etc/puppetlabs/puppet/puppet.conf
+
